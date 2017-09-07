7777208454fb4a84c9a60266729f64edd1532044
SuperSpaceZova <ravalico.giovanni@gmail.com>
2017-09-08 00:03:41 +0200

    giovanni - wk9 day4 - social_network - part 3 - implemented a MODAL DONE

--- src/profilePicUpload.js
+++ src/profilePicUpload.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Modal from './modal.js';
 
 export default class ProfilePicUpload extends React.Component {
 
     constructor( props ) {
         super( props );
+        this.state = { isModalOpen: true };
+    }
+
+    openModal() {
+        this.setState( { isModalOpen: true } );
+    }
+
+    closeModal() {
+        this.setState( { isModalOpen: false } );
     }
 
     render() {
@@ -14,19 +24,26 @@ export default class ProfilePicUpload extends React.Component {
         } = this.props;
 
         return (
-            <div style={{border : 'thin dotted gold'}}>
-                <h4>ProfilePicUploader</h4>
-                <h3 onClick={hideProfilePicUpload}>X</h3>
+
+            <div>
+
+                <Modal isOpen={this.state.isModalOpen} onClose={() => this.closeModal()}>
+
+                    <h4>ProfilePicUploader</h4>
+                    <h3 onClick={hideProfilePicUpload}>X</h3>
 
 
 
-                <label for='profilePic'>Profile Picture</label>
-                <input
-                    id='profilePic'
-                    type="file"
-                    name="profilePic"
-                    required
-                    onChange={uploadProfilePic} />
+                    <label for='profilePic'>Profile Picture</label>
+                    <input
+                        id='profilePic'
+                        type="file"
+                        name="profilePic"
+                        required
+                        onChange={uploadProfilePic} />
+
+                    <p><button onClick={hideProfilePicUpload}>Close</button></p>
+                </Modal>
 
             </div>
         );
